d8d4f073794cb9850d8c56ea58d21cfcc3b39104
Analice de Mello <ana.mello2704@gmail.com>
2022-07-13 20:21:04 -0300

    first commit

---
+++ main.mjs
@@ -0,0 +1,5 @@
+import { mostraIdade, mostraCidade, mostraHobby } from "./funcoes.mjs"; 
+
+console.log(mostraIdade("Alice", 25));
+console.log(mostraCidade("Alice", "Goiânia"));
+console.log(mostraHobby("Alice", "ler livros"))
